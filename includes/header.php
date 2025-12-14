e6c899e0439d83d5a42981ae3cd6ccc93bed68fa
zeinasherif6 <zeina2301499@miuegypt.edu.eg>
2025-12-14 16:06:01 +0200

    .

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@ $wishlist_count = get_wishlist_count();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>The jainz</title>
+    <title>local clothes</title>
     
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
